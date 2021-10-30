cc73b47a5abf6dade2dc4e8ff635bd2ef9149fa8
mkanczuzewski <mkanczuzewski@gmail.com>
2021-10-30 14:55:33 -0400

    really jankey loading technique

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -23,11 +23,39 @@ rows.each(function(i)
 });
 
 $("button").on("click", function(event) {
-  var closestRow = $(this).closest('.row');
-  var textAreaValue = closestRow.find('textarea').val();  
-  var id = closestRow.attr('id');
-  console.log(textAreaValue)
-    localStorage.setItem(id, JSON.stringify(textAreaValue));
+    var closestRow = $(this).closest('.row');
+    var textAreaValue = closestRow.find('textarea').val();  
+    var id = closestRow.attr('id');
+    if (textAreaValue !== null || "")
+    {
+        localStorage.setItem(id, JSON.stringify(textAreaValue));
+    }
 }
 )
 
+    $("#9 .textarea").val(localStorage.getItem(9));
+    $("#10 .textarea").val(localStorage.getItem(10));
+    $("#11 .textarea").val(localStorage.getItem(11));
+    $("#12 .textarea").val(localStorage.getItem(12));
+    $("#13 .textarea").val(localStorage.getItem(13));
+    $("#14 .textarea").val(localStorage.getItem(14));
+    $("#15 .textarea").val(localStorage.getItem(15));
+    $("#16 .textarea").val(localStorage.getItem(16));
+    $("#17 .textarea").val(localStorage.getItem(17));
+//}
+
+//window.onload = function()
+//{
+//    loadTextArea();
+//}
+
+// function loadTextArea() {
+//     if(localStorage !== 'undefined')
+//     {
+//         for(var i = 9; i > 18; i++)
+//         {
+//             $("#i .textarea").val(localStorage.getItem(i))
+                  
+//         }
+//     }
+// }
